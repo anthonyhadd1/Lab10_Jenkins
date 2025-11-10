3e1044d5fb01029569f959a9b2f4b497e356850f
Anthony Haddad <Anthony@example.com>
2025-11-10 20:04:31 +0200

    Initial lab project with tests, lint, coverage, bandit

---
+++ app.py
@@ -0,0 +1,6 @@
+def greet(name: str) -> str:
+    # Update your name here (exactly as in your test)
+    return f"Hello, {name} from Anthony Haddad!"
+
+if __name__ == "__main__":
+    print(greet("World"))
